25ec9c8d25b4b8495d8dbf5bbc8fc444d32a55ef
Jens Hertz <jens.hertz@magnitudespace.com>
2020-10-01 15:44:08 +0200

    Make it compile ...

--- python/__init__.py
+++ python/__init__.py
@@ -26,7 +26,7 @@ description here (python/__init__.py).
 # import swig generated symbols into the ccsds namespace
 try:
 	# this might fail if the module is python-only
-	from ccsds_swig import *
+	from .ccsds_swig import *
 except ImportError:
 	pass
 
